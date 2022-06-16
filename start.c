3ecb41fe7e4f0cb2dfa262835588cd76e9e3a9f8
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-06-16 11:10:42 -0700

    alx

--- start.c
+++ start.c
@@ -1,36 +1,6 @@
 #include "main.h"
 
 /**
- * _environ - Make copy of the environs
- * @obs: shell data
- *
- * Return: Void
- */
-void _environ(op_t *obs)
-{
-	unsigned int i;
-
-	i = 0;
-	while (environ[i])
-		i++;
-
-	obs->env = malloc(sizeof(char *) * (i + 1));
-	if (obs->env == NULL)
-	{
-		write(STDERR_FILENO, "Error\n", 7);
-		return;
-	}
-
-	i = 0;
-	while (environ[i])
-	{
-		obs->env[i] = _strdup(environ[i]);
-		i++;
-	}
-	obs->env[i] = NULL;
-}
-
-/**
  * start - setup shell variables
  * @obs: shell data
  * @argv: argument
@@ -39,13 +9,33 @@ void _environ(op_t *obs)
  */
 void start(op_t *obs, char **argv)
 {
+	unsigned int i;
+
 	obs->av = NULL;
 	obs->cmd = NULL;
 	obs->name = argv[0];
 	obs->exCo = 0;
 	obs->count = 1;
 	obs->pid = _itoa(getpid());
-	_environ(obs);
+
+        i = 0;
+        while (environ[i])
+                i++;
+
+        obs->env = malloc(sizeof(char *) * (i + 1));
+        if (obs->env == NULL)
+        {
+                write(STDERR_FILENO, "Error\n", 7);
+                return;
+        }
+
+        i = 0;
+        while (environ[i])
+        {
+                obs->env[i] = _strdup(environ[i]);
+                i++;
+        }
+        obs->env[i] = NULL;
 }
 
 /**
